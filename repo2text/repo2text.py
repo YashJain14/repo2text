bfca5dbcfcef8a98e1b641d566c8a757484247cd
Yash Jain <yashjain141103@gmail.com>
2024-04-30 14:48:01 +0800

    update

--- repo2text/repo2text.py
+++ repo2text/repo2text.py
@@ -39,12 +39,12 @@ def repo_to_text(repo_url, extension=None):
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python script.py [<extension>] <git url>")
+        print("Usage: repo2text [<extension>] <git url>")
         sys.exit(1)
     # Determine if the first argument is a URL or an extension
     if sys.argv[1].startswith('.'):
         if len(sys.argv) != 3:
-            print("Usage: python script.py [<extension>] <git url>")
+            print("Usage: repo2text <extension> <git url>")
             sys.exit(1)
         extension = sys.argv[1].strip('.').lower()
         repo_url = sys.argv[2]
